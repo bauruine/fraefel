733c3e406b8d93fa8059fac3a1695046300f0f4d
Michael Balsiger <michael.balsiger@swisscom.com>
2011-10-10 01:33:33 +0200

    bug fix stack-level-to deep after_save

--- app/models/purchase_position.rb
+++ app/models/purchase_position.rb
@@ -3,16 +3,18 @@ class PurchasePosition < ActiveRecord::Base
   belongs_to :purchase_order, :class_name => "PurchaseOrder", :foreign_key => "purchase_order_id"
   belongs_to :pallet, :class_name => "Pallet", :foreign_key => "pallet_id"
   
-  after_save :update_purchase_order_date, :update_total_amount
+  after_save :update_purchase_order_date
   after_update :update_weight_total
   
   
   protected
   
   def update_purchase_order_date
+    @cal_total = self.amount * self.quantity
     @purchase_order = self.purchase_order
     @date_for_update = @purchase_order.purchase_positions.order("delivery_date asc").limit(1).first.delivery_date.to_date
     @purchase_order.update_attributes(:delivery_date => @date_for_update)
+    self.update_attributes(:total_amount => @cal_total)
   end
   
   def update_total_amount
