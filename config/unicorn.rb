7b9cca8d745f9ba5f1b11a4afae5bda4dd655b1f
Michael Balsiger <michael.balsiger@swisscom.com>
2011-10-30 11:59:53 +0100

    added unicorn - nginx configs

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/tzhbami7/code/rails/fraefel"
+pid "/home/tzhbami7/code/rails/fraefel/tmp/pids/unicorn.pid"
+stderr_path "/home/tzhbami7/code/rails/fraefel/log/unicorn.log"
+stdout_path "/home/tzhbami7/code/rails/fraefel/log/unicorn.log"
+
+listen "/tmp/unicorn.fraefel.sock"
+worker_processes 3
+timeout 30
