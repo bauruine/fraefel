7e7e35f9a796708e39fffb1083c94b3fee2b21fe
Michael Balsiger <michael.balsiger@swisscom.com>
2011-10-31 15:22:30 +0100

    removed old paperclip testing stuff...

--- app/models/baan_import.rb
+++ app/models/baan_import.rb
@@ -1,5 +1,4 @@
 class BaanImport < ActiveRecord::Base
-  #has_attached_file :baan_upload, :url => ':rails_root/public/system/:attachment/:id/:filename', :path => ':rails_root/public/system/:attachment/:id/:filename'
   has_attached_file :baan_upload
   
   belongs_to :baan_import_group, :class_name => "BaanImportGroup", :foreign_key => "baan_import_group_id"
