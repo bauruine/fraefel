00ae8bbfd728d4c7cc1c1edf2b78f7044929e865
Michael Balsiger <michael.balsiger@swisscom.com>
2011-10-31 11:49:05 +0100

    added attr_accessible for testing...

--- app/models/baan_import.rb
+++ app/models/baan_import.rb
@@ -1,4 +1,6 @@
 class BaanImport < ActiveRecord::Base
   #has_attached_file :baan_upload, :path => ':rails_root/public/system/:attachment/:id/:filename', :processors => []
   belongs_to :baan_import_group, :class_name => "BaanImportGroup", :foreign_key => "baan_import_group_id"
+  
+  attr_accessible(:baan_upload_content_type)
 end
