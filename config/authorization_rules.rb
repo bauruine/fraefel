0095b649b33e046a994ddaa1c3b05f5a44b698a7
Michael Balsiger <michael.balsiger@swisscom.com>
2011-12-14 05:49:39 +0100

    added inventar_rules to auth_file

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -24,6 +24,14 @@ authorization do
     has_permission_on [:baan_imports], :to => [:index, :new, :create, :edit, :update, :import]
   end
   
+  role :inventar do
+    has_permission_on [:articles], :to => [:show, :index, :edit, :edit_multiple, :update_multiple, :search_for, :get_results_for, :calculate_difference_for, :export]
+  end
+  
+  role :inventar_admin do
+    has_permission_on [:articles], :to => [:show, :index, :edit, :edit_multiple, :update_multiple, :search_for, :get_results_for, :calculate_difference_for, :export]
+  end
+  
   role :guest do
     has_permission_on :user_sessions, :to => [:new, :create]
   end
