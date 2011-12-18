5cb24515d5b8a13961d74478ef8f7c76ffd8ad14
Michael Balsiger <michael.balsiger@swisscom.com>
2011-12-18 19:33:35 +0100

    article index.pdf is now dynamic... added some more infos on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,14 @@
 class DashboardsController < ApplicationController
   def show
+    @articles = Article.where(:considered => true)
     @user = current_user
     @open_purchase_orders = PurchaseOrder.where(:delivered => false)
     @bad_purchase_orders = PurchaseOrder.where("delivery_date < #{Date.today - 1.day}")
     @delivered_purchase_orders = PurchaseOrder.where(:delivered => true)
-    @article_rack_group_number_total = Article.group(:rack_group_number).count.size
-    @open_articles = Article.count
+    
+    @article_rack_group_number_total = @articles.group(:rack_group_number).count.size
+    @open_articles = @articles.count
+    @finished_articles = @articles.where("in_stock IS NOT NULL").where("in_stock != ''").count
+    @open_articles_by_group = @articles.where("in_stock IS NULL or in_stock = ''").group(:rack_group_number).count
   end
 end
