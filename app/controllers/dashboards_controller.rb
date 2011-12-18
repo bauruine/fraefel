b88c2075bfa60eeef6ca7a504b8c637650bd0241
Michael Balsiger <michael.balsiger@swisscom.com>
2011-12-18 19:40:40 +0100

    fixed wrong fetching of open articles

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,7 +7,7 @@ class DashboardsController < ApplicationController
     @delivered_purchase_orders = PurchaseOrder.where(:delivered => true)
     
     @article_rack_group_number_total = @articles.group(:rack_group_number).count.size
-    @open_articles = @articles.count
+    @open_articles = @articles.where("in_stock IS NULL or in_stock = ''").count
     @finished_articles = @articles.where("in_stock IS NOT NULL").where("in_stock != ''").count
     @open_articles_by_group = @articles.where("in_stock IS NULL or in_stock = ''").group(:rack_group_number).count
   end
