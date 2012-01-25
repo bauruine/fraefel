51df9eb56c4ef494133ca96faa62aa802b83802d
Michael Balsiger <michael.balsiger@swisscom.com>
2012-01-26 00:52:37 +0100

    testing mass mass with endless scrolling for purchase_orders

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -17,12 +17,24 @@ class PurchaseOrdersController < ApplicationController
 
   def index
     if params[:search].present? && params[:search][:delivered_equals].present? && params[:search][:delivered_equals] == "true"
-      @search = PurchaseOrder.where(:status => "open").where("customer_id IS NOT NULL").search(params[:search])
+      #@search = PurchaseOrder.where(:status => "open").where("customer_id IS NOT NULL").search(params[:search])
+      ##@search = PurchaseOrder.order("shipping_route_id asc, purchase_positions.consignee_full asc, purchase_orders.delivery_date asc, purchase_orders.id asc").includes(:purchase_positions).select("purchase_positions.consignee_full").search(params[:search])
+      
+      @search = PurchaseOrder.order("shipping_route_id asc, customer_id asc, purchase_orders.delivery_date asc, purchase_orders.id asc").search(params[:search])
     else
-      @search = PurchaseOrder.where(:status => "open").where(:delivered => false).where("customer_id IS NOT NULL").search(params[:search])
+      ###@search = PurchaseOrder.order("shipping_route_id asc, purchase_positions.consignee_full asc, purchase_orders.delivery_date asc, purchase_orders.id asc").includes(:purchase_positions).select("purchase_positions.consignee_full").where(:status => "open").where(:delivered => false).where("customer_id IS NOT NULL").search(params[:search])
+      
+      @search = PurchaseOrder.where(:status => "open").where(:delivered => false).where("customer_id IS NOT NULL").order("shipping_route_id asc, customer_id asc, purchase_orders.delivery_date asc, purchase_orders.id asc").search(params[:search])
+      #@search = PurchaseOrder.where(:status => "open").where(:delivered => false).where("customer_id IS NOT NULL").search(params[:search])
     end
     
-    @purchase_orders = @search.order("shipping_route_id asc, purchase_positions.consignee_full asc, purchase_orders.delivery_date asc, purchase_orders.id asc").includes(:purchase_positions).select("purchase_positions.consignee_full")
+    #@purchase_orders = @search.order("shipping_route_id asc, purchase_positions.consignee_full asc, purchase_orders.delivery_date asc, purchase_orders.id asc").includes(:purchase_positions).select("purchase_positions.consignee_full")
+    @purchase_orders = @search.page(params[:page]).per(15)
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
   def edit
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,19 +3,19 @@
 //netscape.security.PrivilegeManager.enablePrivilege("UniversalFileRead");
 
 if (navigator.appName == "Microsoft Internet Explorer") {
-	var ua = navigator.userAgent;
-	var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-	var rv = parseFloat(re.exec(ua)[1]);
-	if (rv < 9) {
-		document.createElement('header');
-		document.createElement('hgroup');
-		document.createElement('nav');
-		document.createElement('menu');
-		document.createElement('section');
-		document.createElement('article');
-		document.createElement('aside');
-		document.createElement('footer');
-	};
+  var ua = navigator.userAgent;
+  var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+  var rv = parseFloat(re.exec(ua)[1]);
+  if (rv < 9) {
+    document.createElement('header');
+    document.createElement('hgroup');
+    document.createElement('nav');
+    document.createElement('menu');
+    document.createElement('section');
+    document.createElement('article');
+    document.createElement('aside');
+    document.createElement('footer');
+  };
 };
 
 $(document).ready(function(){
@@ -32,66 +32,72 @@ $(document).ready(function(){
       return true;
   });
 
-	$('.date_picker').datepicker({ dateFormat: 'yy-mm-dd' });
+  $('.date_picker').datepicker({ dateFormat: 'yy-mm-dd' });
 
-	$('div[data-type=modal]').dialog({ autoOpen: false, modal: true, draggable: false, resizable: false });
-	
-	$('a[data-type=modal]').click(function() {
-		var remote_url = $(this).attr("href");
-		$('div[data-type=modal]').load(remote_url + ' form');
-		if ($(this).attr('data-modal_width') != undefined) {
-			$( "div[data-type=modal]" ).dialog( "option", "width", 500 );
-		};
-		$('div[data-type=modal]').dialog('open');
-		$('div[data-type=modal]').dialog("option", "position", "center");
-		
-		return false;
-	});
-	
-	$('a[data-type=modal_index]').click(function() {
-		var remote_url = $(this).attr("href");
-		$('div[data-type=modal]').load(remote_url + ' table');
-		if ($(this).attr('data-modal_width') != undefined) {
-			$( "div[data-type=modal]" ).dialog( "option", "width", 1424 );
-		};
-		$('div[data-type=modal]').dialog('open');
-		$('div[data-type=modal]').dialog("option", "position", "center");
-		
-		return false;
-	});
-	
-	$(window).resize(function() {
-		$('div[data-type=modal]').dialog("option", "position", "center");
-	});
-	
-	if ($('form[data-submit_handler=true]').size() != 0) {
-		$("form[data-submit_handler=true]").find($("input[type=submit]")).attr('disabled', 'disabled');
-		
-		$("form[data-submit_handler=true]").find($("input[type=checkbox]")).change(function(){
-			if ($("form[data-submit_handler=true]").find($("input[type=checkbox]:checked")).length >= 1) {
-				$("form[data-submit_handler=true]").find($("input[type=submit]")).removeAttr('disabled');
-			} else {
-				$("form[data-submit_handler=true]").find($("input[type=submit]")).attr('disabled', 'disabled');
-			};
-		});
-		
-	};
-	
-	if ($("form[data-tab]").size() != 0) {
-		$("form:not([data-tab=filter])").hide()
-	};
-	
-	$("a[data-type=tab]").click(function() {
-		var warpToTab = $(this).attr("data-warp");
-		var currentForm = $("form[data-tab]")
-		$("form:not(:hidden)").hide();
-		$("form[data-tab=" + warpToTab + "]").show();
-		return false;
-	});
-/*	
-	var faye = new Faye.Client('http://localhost:9292/faye');
-	faye.subscribe("/messages/new", function(data) {
-		alert(data);
-	});
-*/	
+  $('div[data-type=modal]').dialog({ autoOpen: false, modal: true, draggable: false, resizable: false });
+  
+  $('a[data-type=modal]').click(function() {
+    var remote_url = $(this).attr("href");
+    $('div[data-type=modal]').load(remote_url + ' form');
+    if ($(this).attr('data-modal_width') != undefined) {
+      $( "div[data-type=modal]" ).dialog( "option", "width", 500 );
+    };
+    $('div[data-type=modal]').dialog('open');
+    $('div[data-type=modal]').dialog("option", "position", "center");
+    
+    return false;
+  });
+  
+  $('a[data-type=modal_index]').click(function() {
+    var remote_url = $(this).attr("href");
+    $('div[data-type=modal]').load(remote_url + ' table');
+    if ($(this).attr('data-modal_width') != undefined) {
+      $( "div[data-type=modal]" ).dialog( "option", "width", 1424 );
+    };
+    $('div[data-type=modal]').dialog('open');
+    $('div[data-type=modal]').dialog("option", "position", "center");
+    
+    return false;
+  });
+  
+  $(window).resize(function() {
+    $('div[data-type=modal]').dialog("option", "position", "center");
+  });
+  
+  if ($('form[data-submit_handler=true]').size() != 0) {
+    $("form[data-submit_handler=true]").find($("input[type=submit]")).attr('disabled', 'disabled');
+    
+    $("form[data-submit_handler=true]").find($("input[type=checkbox]")).change(function(){
+      if ($("form[data-submit_handler=true]").find($("input[type=checkbox]:checked")).length >= 1) {
+        $("form[data-submit_handler=true]").find($("input[type=submit]")).removeAttr('disabled');
+      } else {
+        $("form[data-submit_handler=true]").find($("input[type=submit]")).attr('disabled', 'disabled');
+      };
+    });
+    
+  };
+  
+  if ($("form[data-tab]").size() != 0) {
+    $("form:not([data-tab=filter])").hide()
+  };
+  
+  $("a[data-type=tab]").click(function() {
+    var warpToTab = $(this).attr("data-warp");
+    var currentForm = $("form[data-tab]")
+    $("form:not(:hidden)").hide();
+    $("form[data-tab=" + warpToTab + "]").show();
+    return false;
+  });
+
+  if ($("nav.pagination").length) {
+    $("nav.pagination").hide();
+    $(window).scroll(function () {
+      var url = $("nav.pagination span.next a").attr('href');
+      if (url && $(window).scrollTop() > ($(document).height() - $(window).height() - 50)) {
+        $("nav.pagination").text("Fetching...");
+        $.getScript(url);
+      };
+    });
+  };
+  
 });
