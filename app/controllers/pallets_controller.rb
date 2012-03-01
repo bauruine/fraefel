b61f3349102ab8ced4720e9a079f884ad2e5052f
Michael Balsiger <michael.balsiger@swisscom.com>
2012-03-01 18:57:56 +0100

    usability restored...

--- app/controllers/pallets_controller.rb
+++ app/controllers/pallets_controller.rb
@@ -132,7 +132,7 @@ class PalletsController < ApplicationController
     end
     params[:quantity_with_ids].each do |k, v|
       purchase_position = PurchasePosition.find(k.to_i)
-      pallet_purchase_position_assignment = PalletPurchasePositionAssignment.where(:pallet => @pallet, :purchase_position => purchase_position).first
+      pallet_purchase_position_assignment = PalletPurchasePositionAssignment.where(:pallet_id => @pallet.id, :purchase_position_id => purchase_position.id).first
       pallet_purchase_position_assignment.update_attributes(:quantity => v.to_i, :amount => (pallet_purchase_position_assignment.purchase_position.amount * v.to_i), :weight => (pallet_purchase_position_assignment.purchase_position.weight_single * v.to_i)) if pallet_purchase_position_assignment.present?
     end
     redirect_to(:back)
