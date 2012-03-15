94d20a435d225edf8a5080feb12b72183d9605f7
Michael Balsiger <michael.balsiger@swisscom.com>
2012-03-15 13:17:13 +0100

    changed some utf errors

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -4,7 +4,7 @@ class Notifications < ActionMailer::Base
   def password_reset_instructions(user)
     @user = User.find(user)
     @edit_password_reset_url = edit_password_reset_url(@user.perishable_token)
-    mail to: @user.email, from: "api.fraefel@gmail.com", subject: "Instruktionen zum setzen/wiederherstellen des Passwords", encoding: "utf-8"
+    mail to: @user.email, from: "api.fraefel@gmail.com", subject: "Instruktionen zum setzen/wiederherstellen des Passwords"
   end
   
   def user_signed_up(user)
