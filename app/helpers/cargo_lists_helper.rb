3c64130a91b00e4a18ff6e2668c7f3ced481e5cf
Michael Balsiger <michael.balsiger@swisscom.com>
2012-10-03 22:54:30 +0200

    trying -

--- app/helpers/cargo_lists_helper.rb
+++ app/helpers/cargo_lists_helper.rb
@@ -1,5 +1,5 @@
 module CargoListsHelper
   def pdf_title(cargo_list_id, title)
-    return "versand_#{CargoList.find(cargo_list_id).id}_#{CargoList.find(cargo_list_id).pick_up_time_earliest.to_date.to_formatted_s(:pdf)}_#{title}"
+    return "versand-#{CargoList.find(cargo_list_id).id}-#{CargoList.find(cargo_list_id).pick_up_time_earliest.to_date.to_formatted_s(:pdf)}-#{title}"
   end
 end
