b6df1f33aeda50485a413a68c1aae526e52f2102
Michael Balsiger <michael.balsiger@swisscom.com>
2012-11-02 15:44:27 +0100

    fixed some minor bugs in views. commented some lines

--- app/controllers/delivery_rejections_controller.rb
+++ app/controllers/delivery_rejections_controller.rb
@@ -1,6 +1,6 @@
 class DeliveryRejectionsController < ApplicationController
   filter_access_to :all
-  
+
   def show
     @delivery_rejection = DeliveryRejection.find(params[:id])
     @cargo_lists = @delivery_rejection.cargo_lists
@@ -14,26 +14,26 @@ class DeliveryRejectionsController < ApplicationController
     @delivery_address = @delivery_rejection.delivery_address
     @invoice_address = @delivery_rejection.invoice_address
     @pick_up_address = @delivery_rejection.pick_up_address
-    
+
     @pallet_purchase_position_assignments = PalletPurchasePositionAssignment.select("DISTINCT `pallet_purchase_position_assignments`.*").joins(:pallet => :delivery_rejection).where("delivery_rejections.id" => @delivery_rejection.id)
     @pallet_types = PalletType.joins(:pallets).where("pallets.delivery_rejection_id" => @delivery_rejection.id)
-    
+
     respond_to do |format|
       format.html
       format.pdf do
-        render( 
-          :pdf => "Kein Titel-#{Date.today}",
-          :wkhtmltopdf => '/usr/bin/wkhtmltopdf',
-          :layout => 'pdf.html',
-          :show_as_html => params[:debug].present?,
-          :orientation => 'Portrait',
-          :encoding => 'UTF-8'
+        render(
+            :pdf => "Kein Titel-#{Date.today}",
+            :wkhtmltopdf => '/usr/bin/wkhtmltopdf',
+            :layout => 'pdf.html',
+            :show_as_html => params[:debug].present?,
+            :orientation => 'Portrait',
+            :encoding => 'UTF-8'
         )
         #purchase_positions.group(:commodity_code_id).sum(:amount)
       end
     end
   end
-  
+
   def index
     @search = DeliveryRejection.search(params[:q] || {:closed_eq => false})
     @delivery_rejections = @search.result.order("delivery_rejections.id DESC")
@@ -44,57 +44,58 @@ class DeliveryRejectionsController < ApplicationController
     @statuses = Status.select("DISTINCT `statuses`.*").joins(:delivery_rejections).where("delivery_rejections.id" => @delivery_rejection_ids)
     @customers = Customer.select("DISTINCT `customers`.*").joins(:delivery_rejections).where("delivery_rejections.id" => @delivery_rejection_ids)
   end
-  
+
   def new
     @delivery_rejection = DeliveryRejection.new
     @comment = @delivery_rejection.comments.build
     @search = PurchasePosition.search(params[:q])
   end
-  
+
   def create
     @delivery_rejection = DeliveryRejection.new(params[:delivery_rejection])
     #@comment = @delivery_rejection.comments.build
     @search = PurchasePosition.search(params[:q])
-    
+
     if @delivery_rejection.save
       redirect_to delivery_rejection_url(@delivery_rejection)
     else
       render 'new'
     end
   end
-  
+
   def edit
     @delivery_rejection = DeliveryRejection.find(params[:id])
     @comment = @delivery_rejection.comments.build
 
     @delivery_rejection.build_referee if @delivery_rejection.referee.nil?
-    @delivery_rejection.build_new_delivery_address if @delivery_rejection.new_delivery_address.nil?
-    @delivery_rejection.build_new_pick_up_address if @delivery_rejection.new_pick_up_address.nil?
-    @delivery_rejection.build_new_invoice_address if @delivery_rejection.new_invoice_address.nil?
-    
+    if @can_use_beta
+      @delivery_rejection.build_new_delivery_address if @delivery_rejection.new_delivery_address.nil?
+      @delivery_rejection.build_new_pick_up_address if @delivery_rejection.new_pick_up_address.nil?
+      @delivery_rejection.build_new_invoice_address if @delivery_rejection.new_invoice_address.nil?
+    end
     @delivery_addresses = Address.where(:category_id => 3).order("company_name ASC")
     @invoice_addresses = Address.where(:category_id => 14).order("company_name ASC")
     @pick_up_addresses = Address.where(:category_id => 4).order("company_name ASC")
-    
+
     @search = PurchasePosition.search(params[:q])
   end
-  
+
   def update
     @delivery_rejection = DeliveryRejection.find(params[:id])
     #@comment = @delivery_rejection.comments.build
     #@search = PurchasePosition.search(params[:search])
-    
+
     if @delivery_rejection.update_attributes(params[:delivery_rejection])
       redirect_to delivery_rejection_path(@delivery_rejection)
     else
       render 'edit'
     end
   end
-  
+
   def destroy
-    
+
   end
-  
+
   def assign_positions
     @delivery_rejection = DeliveryRejection.where(:id => params[:delivery_rejection_id]).first
     if params[:pallet_id].present?
@@ -111,10 +112,11 @@ class DeliveryRejectionsController < ApplicationController
       purchase_position = PurchasePosition.find(k.to_i)
       pallet_purchase_position_assignment = PalletPurchasePositionAssignment.where(:pallet_id => @pallet.id, :purchase_position_id => purchase_position.id).first
       pallet_purchase_position_assignment.update_attributes(:reduced_price => (((pallet_purchase_position_assignment.purchase_position.amount * v.to_i) / 100) * @delivery_rejection.discount), :quantity => v.to_i, :amount => (pallet_purchase_position_assignment.purchase_position.amount * v.to_i), :weight => (pallet_purchase_position_assignment.purchase_position.weight_single * v.to_i)) if pallet_purchase_position_assignment.present?
+      pallet_purchase_position_assignment.very_ugly_patcher
     end
     redirect_to(:back)
   end
-  
+
   def remove_positions
     @pallet = Pallet.find(params[:pallet_id])
     @purchase_positions = PurchasePosition.where(:id => params[:purchase_position_ids])
@@ -124,7 +126,7 @@ class DeliveryRejectionsController < ApplicationController
     end
     redirect_to(:back)
   end
-  
+
   def search
     @cargo_list = CargoList.where("purchase_orders.baan_id = ?", params[:purchase_order_baan_id]).includes(:pallets => :purchase_orders)
     respond_to do |format|
--- app/models/pallet_purchase_position_assignment.rb
+++ app/models/pallet_purchase_position_assignment.rb
@@ -1,7 +1,7 @@
 class PalletPurchasePositionAssignment < ActiveRecord::Base
   belongs_to :pallet, :class_name => "Pallet", :foreign_key => "pallet_id"
   belongs_to :purchase_position, :class_name => "PurchasePosition", :foreign_key => "purchase_position_id"
-  
+
   after_create :update_purchase_position_counter
   after_create :update_purchase_order_calculation
   after_create :update_pallet_level_3
@@ -9,7 +9,7 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
   after_create :update_pallet_shipping_route_id
   after_destroy :update_purchase_position_counter
   after_destroy :handle_assignments
-  
+
   def self.fix_amount_and_weight
     self.all.each do |p_p_p_a|
       if p_p_p_a.purchase_position.present?
@@ -17,7 +17,7 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
       end
     end
   end
-  
+
   def self.fix_quantity_for_cargo_list
     self.where("pallets.delivery_rejection_id IS NULL").includes(:pallet).each do |p_p_p_a|
       if p_p_p_a.purchase_position.present?
@@ -25,7 +25,7 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
       end
     end
   end
-  
+
   def self.patch_gross_net_value_discount
     self.all.each do |p_p_p_a|
       gross_price = p_p_p_a.purchase_position.gross_price.present? ? p_p_p_a.purchase_position.gross_price : p_p_p_a.purchase_position.amount
@@ -34,15 +34,13 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
       p_p_p_a.update_attributes(:gross_price => gross_price, :net_price => net_price, :value_discount => value_discount)
     end
   end
-  
+
   def self.patch_pallet_purchase_position_counter
     self.select("DISTINCT `pallet_purchase_position_assignments`.*").joins(:pallet, :purchase_position).readonly(false).each do |pallet_purchase_position_assignment|
       pallet_purchase_position_assignment.pallet.update_attribute("purchase_position_counter", pallet_purchase_position_assignment.quantity)
     end
   end
 
-  private
-  
   def update_pallet_shipping_route_id
     if self.pallet.present? && self.purchase_position.present?
       @purchase_orders = self.pallet.purchase_orders
@@ -51,7 +49,7 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
       self.pallet.update_attribute("shipping_route_id", @shipping_route_id)
     end
   end
-  
+
   def update_pallet_level_3
     if self.pallet.present? && self.purchase_position.present?
       @purchase_orders = self.pallet.purchase_orders
@@ -60,7 +58,7 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
       self.pallet.update_attribute("level_3", @level_3_id)
     end
   end
-  
+
   def update_pallet_zip_location_id
     if self.pallet.present? && self.purchase_position.present?
       @purchase_positions = self.pallet.purchase_positions
@@ -69,24 +67,32 @@ class PalletPurchasePositionAssignment < ActiveRecord::Base
       self.pallet.update_attribute("zip_location_id", @zip_location_id)
     end
   end
-  
+
   def update_purchase_position_counter
     if self.pallet.present? && self.purchase_position.present?
       @pallet_purchase_position_assignments = PalletPurchasePositionAssignment.where(:pallet_id => self.pallet.id)
       self.pallet.update_attribute("purchase_position_counter", @pallet_purchase_position_assignments.sum("quantity"))
     end
   end
-  
+
   def update_purchase_order_calculation
     if self.purchase_position.present?
       self.purchase_position.purchase_order.calculation.update_attribute(:total_pallets, self.purchase_position.purchase_order.pallets.count)
     end
   end
-  
+
   def handle_assignments
     if self.pallet.purchase_position_counter == 0
       self.pallet.update_attributes(:cargo_list_id => nil, :delivery_rejection_id => nil)
     end
   end
-  
+
+  def very_ugly_patcher
+    self.update_purchase_position_counter
+    self.update_purchase_order_calculation
+    self.update_pallet_level_3
+    self.update_pallet_zip_location_id
+    self.update_pallet_shipping_route_id
+
+  end
 end
