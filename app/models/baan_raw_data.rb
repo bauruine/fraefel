971c5cc79def8612c4132f4e1b30838721752766
Michael Balsiger <michael.balsiger@outlook.com>
2014-12-21 12:30:39 -0800

    Importing new Address Additionals

--- app/models/baan_raw_data.rb
+++ app/models/baan_raw_data.rb
@@ -1,8 +1,8 @@
 class BaanRawData < ActiveRecord::Base
   attr_accessor :should_preload
-  
+
   belongs_to :baan_import, :class_name => "BaanImport", :foreign_key => "baan_import_id"
-  
+
   after_create :create_import_purchase_order, :if => :should_preload
   after_create :create_import_purchase_position, :if => :should_preload
   after_create :create_import_address, :if => :should_preload
@@ -11,7 +11,7 @@ class BaanRawData < ActiveRecord::Base
   after_create :create_import_zip_location, :if => :should_preload
   after_create :create_import_commodity_code, :if => :should_preload
   after_create :create_import_category, :if => :should_preload
-  
+
   def self.import(arg, preloading = true)
     @baan_import = BaanImport.find(arg)
     preloading = preloading
@@ -21,7 +21,7 @@ class BaanRawData < ActiveRecord::Base
     csv_file.each do |row|
       @baan_raw_attributes = {}
       # added new lines 82 83 84
-      for i in 0..84 do
+      for i in 0..87 do
         @baan_raw_attributes.merge!("baan_#{i}".to_sym => row[i].to_s.undress)
       end
       @baan_raw_attributes.merge!(:baan_import_id => @baan_import.id)
@@ -31,9 +31,9 @@ class BaanRawData < ActiveRecord::Base
         baan_raw_data.save
       end
     end
-    
+
   end
-  
+
   def self.import_cancelled(arg)
     @baan_import = BaanImport.find(arg)
     csv_file_path = @baan_import.baan_upload.path
@@ -43,21 +43,21 @@ class BaanRawData < ActiveRecord::Base
       BaanRawData.find_or_create_by_baan_2_and_baan_4_and_baan_import_id(:baan_2 => row[1].to_s.undress, :baan_4 => row[2].to_s.undress, :baan_import_id => @baan_import.id)
     end
   end
-  
+
   protected
-  
+
   def create_import_purchase_order
     unless Import::PurchaseOrder.find(:baan_id => self.baan_2).present?
       Import::PurchaseOrder.create(:baan_id => self.baan_2)
     end
   end
-  
+
   def create_import_purchase_position
     unless Import::PurchasePosition.find(:baan_id => "#{self.baan_2}-#{self.baan_4}").present?
       Import::PurchasePosition.create(:baan_id => "#{self.baan_2}-#{self.baan_4}")
     end
   end
-  
+
   def create_import_address
     unless Import::Address.find(:unique_id => Digest::MD5.hexdigest("#{self.baan_55}-8")).present?
       Import::Address.create(:baan_id => self.baan_55, :category_id => "8", :unique_id => Digest::MD5.hexdigest("#{self.baan_55}-8"))
@@ -69,35 +69,35 @@ class BaanRawData < ActiveRecord::Base
       Import::Address.create(:baan_id => self.baan_71, :category_id => "10", :unique_id => Digest::MD5.hexdigest("#{self.baan_71}-10"))
     end
   end
-  
+
   def create_import_customer
     unless Import::Customer.find(:baan_id => self.baan_6).present?
       Import::Customer.create(:baan_id => self.baan_6)
     end
   end
-  
+
   def create_import_shipping_route
     unless Import::ShippingRoute.find(:baan_id => self.baan_21).present?
       Import::ShippingRoute.create(:baan_id => self.baan_21)
     end
   end
-  
+
   def create_import_zip_location
     unless Import::ZipLocation.find(:baan_id => self.baan_35).present?
       Import::ZipLocation.create(:baan_id => self.baan_35)
     end
   end
-  
+
   def create_import_commodity_code
     unless Import::CommodityCode.find(:baan_id => self.baan_0).present?
       Import::CommodityCode.create(:baan_id => self.baan_0)
     end
   end
-  
+
   def create_import_category
     unless Import::Category.find(:unique_id => Digest::MD5.hexdigest(%Q(#{self.baan_81}-purchase_order))).present?
       Import::Category.create(:baan_id => self.baan_81, :categorizable_type => "purchase_order", :unique_id => Digest::MD5.hexdigest(%Q(#{self.baan_81}-purchase_order)))
     end
   end
-  
+
 end
