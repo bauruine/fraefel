d449f2c39c05f0abcb08e42a0fff0e9db4e6562e
Michael Balsiger <michael.balsiger@outlook.com>
2014-12-21 11:47:20 -0800

    Removed Text ("Bestätigung Zoll")

--- app/pdfs/proforma_invoice_document.rb
+++ app/pdfs/proforma_invoice_document.rb
@@ -12,7 +12,7 @@ class ProformaInvoiceDocument < Prawn::Document
     @pallet_purchase_position_assignments = PalletPurchasePositionAssignment.where("cargo_lists.id = ?", @cargo_list.id).includes(:pallet => [:cargo_list], :purchase_position => [:commodity_code])
     @pallets = Pallet.where("cargo_lists.id = ?", @cargo_list.id).includes(:cargo_list)
     @pallet_types = PalletType.where("cargo_lists.id = ?", @cargo_list.id).includes(:pallets => :cargo_list)
-    
+
     font_size 8
     #move_down 97
     bounding_box [0, 690], :width => 261.64, :height => 220 do
@@ -29,7 +29,7 @@ class ProformaInvoiceDocument < Prawn::Document
     shipment_data
     shipment_declaration
   end
-  
+
   def invoice_address
     text "Rechnungs Adresse", style: :bold
     move_down 11
@@ -39,7 +39,7 @@ class ProformaInvoiceDocument < Prawn::Document
       row(0).columns(0).font_style = :bold
     end
   end
-  
+
   def invoice_address_table
     [
       ["Schweizerbad GmbH", ""],
@@ -48,20 +48,20 @@ class ProformaInvoiceDocument < Prawn::Document
       ["Deutschland", ""],
       ["Eori Nr. DE 7662858", ""]
     ]
-    
+
   end
-  
+
   def delivery_address
     text "Liefer Adresse", style: :bold
     move_down 11
-    
+
     table delivery_address_table do
       self.width = 261.64
       self.cell_style = { :padding_left => 0, :padding_top => 1, :padding_bottom => 1, :borders => [] }
       column(0).width = 80
     end
   end
-  
+
   def delivery_address_table
     [
       ["Firma:", "#{@address.company_name}"],
@@ -71,19 +71,19 @@ class ProformaInvoiceDocument < Prawn::Document
       ["Land:", "Deutschland"]
     ]
   end
-  
+
   def address_of_sender
     text "Sender Adresse", style: :bold
     move_down 11
-    
+
     table address_of_sender_table do
       self.width = 261.64
       self.cell_style = { :padding_left => 0, :padding_top => 1, :padding_bottom => 1, :borders => [] }
       column(0).width = 80
     end
-    
+
   end
-  
+
   def address_of_sender_table
     [
       ["Firma:", " Fraefel AG"],
@@ -96,34 +96,34 @@ class ProformaInvoiceDocument < Prawn::Document
       ["DE-Zollnummer.:", "6207308"]
     ]
   end
-  
+
   def proforma_invoice
     table [["Proforma Rechnung", "Nur für Zollzwecke."], ["Rechnungs Datum:", "#{Date.today.to_formatted_s(:swiss_date)}"]] do
       self.width = 523.28
       self.cell_style = { :padding_left => 0, :borders => [] }
-      
+
       row(0).columns(0).font_style = :bold
       column(0).width = 130
       #row(1).width = 423.28
     end
     stroke_horizontal_rule
     move_down 10
-    
+
     table [["Warenbezeichnung", "Warentarifnummer", "Stk.", "Gewicht", "Warenwert"]] + foobar_items + foobar2_items do
       row(0).font_style = :bold
       row(0).columns(0..7).padding = [0, 5, 0, 0]
-      
+
       self.width = 523.28
       self.cell_style = { :padding_left => 0, :borders => [] }
-      
+
       row(self.row_length - 2).columns(3).font_style = :bold
       row(self.row_length - 1).columns(3).font_style = :bold
-      
+
     end
     stroke_horizontal_rule
-    
+
   end
-  
+
   def foobar_items
     @purchase_positions.group(:commodity_code_id).count.map do |k, v|
       [
@@ -135,14 +135,14 @@ class ProformaInvoiceDocument < Prawn::Document
       ]
     end
   end
-  
+
   def foobar2_items
     [
       ["", "", "", "Rechnungswert", "#{@pallet_purchase_position_assignments.sum(:amount) }"],
       ["", "", "", "Währung", "EUR"]
     ]
   end
-  
+
   def foobar3_items
     [
       ["Transport:", "Lebert", "Lieferbedingung:", "frei Haus, verzollt, versteuert (DDP)"],
@@ -153,48 +153,48 @@ class ProformaInvoiceDocument < Prawn::Document
       ["Coli:", "#{@pallet_types.where("pallet_types.description = ?", "coli").count}", "", ""]
     ]
   end
-  
+
   def foobar4_items
     [
       ["Ort / Datum:", "Lütisburg-Station / #{Date.today.to_formatted_s(:swiss_date)}", "Unterschrift:", ""],
       ["", "", "Name:", "#{User.current.full_name}"]
     ]
   end
-  
+
   def shipment_data
     move_down 10
-    
+
     text "Versand Daten", style: :bold
-    
+
     move_down 10
-    
+
     table foobar3_items do
       self.width = 523.28
       self.cell_style = { :padding_left => 0, :padding_bottom => 0, :padding_top => 3, :borders => [] }
       column(0).width = 130
       row(0).columns(1).font_style = :bold
     end
-    
+
     move_down 5
     stroke_horizontal_rule
   end
-  
+
   def shipment_declaration
     move_down 10
-    text "Der Ausführer ( Ermächtigter Ausführer; Bewilligungs-Nr.4218 ) der Waren, auf die sich dieses", style: :bold
+    # text "Der Ausführer ( Ermächtigter Ausführer; Bewilligungs-Nr.4218 ) der Waren, auf die sich dieses", style: :bold
     move_down 5
-    text "Handelspapier bezieht, erklärt, dass diese Waren, soweit nicht anders angegeben, präferenzbegünstigte", style: :bold
+    # text "Handelspapier bezieht, erklärt, dass diese Waren, soweit nicht anders angegeben, präferenzbegünstigte", style: :bold
     move_down 5
-    text "schweizerische Ursprungswaren sind.", style: :bold
+    # text "schweizerische Ursprungswaren sind.", style: :bold
     move_down 15
-    
+
     table foobar4_items do
       self.width = 523.28
       self.cell_style = { :padding_left => 0, :padding_bottom => 0, :padding_top => 3, :borders => [] }
       column(0).width = 130
       row(0).columns(3).borders = [:bottom]
     end
-    
+
   end
-  
+
 end
