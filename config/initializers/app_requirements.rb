277c06f8260a8731a8d4d98b5c8f412dd21f1f79
Michael Balsiger <michael.balsiger@outlook.com>
2015-03-24 00:28:07 +0100

    Added pluck_all method
    * acts like pluck but accepts multiple columns

--- config/initializers/app_requirements.rb
+++ config/initializers/app_requirements.rb
@@ -2,7 +2,7 @@ class String
   def undress
     self.chomp.lstrip.rstrip
   end
-  
+
   def to_md5
     return Digest::MD5.hexdigest(dup)
   end
@@ -34,13 +34,35 @@ class Hash
       " #{attrs.sort * ' '}".html_safe unless attrs.empty?
     end
   end
-  
+
   def to_md5(*except_keys)
     return Digest::MD5.hexdigest(dup.except!(*except_keys).to_s)
   end
-  
+
 end
 
+module ActiveRecord
+  class Relation
+    def pluck_all(*args)
+      args.map! do |column_name|
+        if column_name.is_a?(Symbol) && column_names.include?(column_name.to_s)
+          "#{connection.quote_table_name(table_name)}.#{connection.quote_column_name(column_name)}"
+        else
+          column_name.to_s
+        end
+      end
+
+      relation = clone
+      relation.select_values = args
+      klass.connection.select_all(relation.arel).map! do |attributes|
+        initialized_attributes = klass.initialize_attributes(attributes)
+        attributes.each do |key, attribute|
+          attributes[key] = klass.type_cast_attribute(key, initialized_attributes)
+        end
+      end
+    end
+  end
+end
 
 Ransack.configure do |config|
   config.add_predicate 'equals', :arel_predicate => 'eq', :validator => proc {|v| v.present?}
